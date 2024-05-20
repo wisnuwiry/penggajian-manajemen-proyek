13ed313ff68c32dbfb68e21eef460d6f1672d4e5
Wisnu G. Saputra <wisnuwiry@gmail.com>
2024-05-20 22:48:03 +0700

    Update copy

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
-                    <p>INi adalah halaman karyawan</p>
+                    <p>Ini adalah halaman karyawan</p>
                 </div>
             </div>
         </div>
